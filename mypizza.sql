72b14f1e812a3da70a2a90aa9f0ce649a3e7cbaa
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-05-05 22:16:25 +0900

    Add files via upload

---
+++ mypizza.sql
@@ -0,0 +1,27 @@
+create table mypizza (
+id int not null auto_increment,
+name varchar(20) not null,
+large int not null,
+small int not null,
+photo varchar(50),
+primary key(id));
+
+insert into user values(); 
+
+insert into mypizza(name,large,small,photo) values("치즈 케이크롤",33900,28000,"pizza1.jpg"),
+("치즈 앤 그릴드 비프", 33900, 28000, "pizza2.jpg"),
+("슈퍼시드 앤 스테이크롤", 33900, 28000, "pizza3.jpg"),
+
+;
+
+insert into mypizza(name,large,small,photo) values("치즈 앤 그릴드 비프", 33900, 28000, "pizza2.jpg");
+
+insert into mypizza(name,large,small,photo) values("슈퍼시드 앤 스테이크롤", 33900, 28000, "pizza3.jpg");
+
+insert into mypizza(name,large,small,photo) values("더블크러스트 치즈멜팅롤", 33900, 28000, "pizza4.jpg");
+
+insert into mypizza(name,large,small,photo) values("와규 앤 비스테카", 33900, 28000, "pizza5.jpg");
+
+insert into mypizza(name,large,small,photo) values("킹 프론 씨푸드", 33900, 28000, "pizza6.jpg");
+
+insert into mypizza(name,large,small,photo) values("직화스테이크", 33900, 28000, "pizza7.jpg");
